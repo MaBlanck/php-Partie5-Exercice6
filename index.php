a687c0e372c5cf96d0e7b6fda5dcc9da178f4902
Max <maxblanck@outlook.fr>
2020-12-09 12:06:39 +0100

    php Partie 5  Exercice 6

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php $departments = array(
+    02 => 'Aisne',
+    59 => 'Nord' ,
+    60 => 'Oise' ,
+    62 =>'Pas-de-Calais',
+    80 =>'Somme');
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 6 Partie 5</title>
+</head>
+<body>
+    <h1>Exercice 6 Partie 5</h1>
+    <p><?= $departments[59] ?></p>
+</body>
+</html>
